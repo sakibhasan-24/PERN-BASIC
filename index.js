616b92a1095983e29f73b63f0744d9d7c24e2457
sakibhasan-24 <sakibhasan7724@gmail.com>
2026-03-23 23:49:58 +0600

    create a vanila server

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("PERN SETUP")
+// console.log("PERN SETUP")
+// import os from 'os'
+
+// console.log(os.platform())
+
+
+import http from 'http'
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end("Hello Server   ")
+
+
+})
+
+server.listen(3000, () => {
+    console.log("Server is running on port http://localhost:3000")
+})
